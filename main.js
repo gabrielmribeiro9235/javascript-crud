b803aba12f7993fe3b7841e94bb158364ce26bbd
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2025-12-29 12:01:47 -0300

    refactor: just adjusting the format

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const contents = [];
 let nextId = 1;
+
 const createPost = (owner, content) => {
   contents.push({ id: nextId++, owner: owner, content: content });
 };
@@ -10,21 +11,21 @@ const getPostById = (id) => {
 };
 
 const updatePost = (id, update) => {
-  const foundPost = contents.find(post => post.id === id);
-  if(foundPost) {
+  const foundPost = contents.find((post) => post.id === id);
+  if (foundPost) {
     foundPost.content = update;
-    console.log("Atualizado com sucesso!")
+    console.log("Atualizado com sucesso!");
   } else {
     console.log("Post não encontrado!");
   }
 };
 
 const deletePost = (id) => {
-    const index = contents.findIndex(post => post.id === id);
-    if(index !== -1) {
-        contents.splice(index, 1);
-        console.log("Post deletado!")
-    } else {
-        console.log("Post não encontrado!");
-    }
+  const index = contents.findIndex((post) => post.id === id);
+  if (index !== -1) {
+    contents.splice(index, 1);
+    console.log("Post deletado!");
+  } else {
+    console.log("Post não encontrado!");
+  }
 };
