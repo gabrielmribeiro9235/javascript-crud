e4a60022cfca3f1b85eadd4caf894a08b96a3049
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2025-12-29 11:58:46 -0300

    feat: create the function getPostById

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ let nextId = 1;
 const createPost = (owner, content) => {
   contents.push({ id: nextId++, owner: owner, content: content });
 };
+
+const getPostById = (id) => {
+  const foundPost = contents.find((post) => post.id === id);
+  return foundPost || "Não encontrado!";
+};
