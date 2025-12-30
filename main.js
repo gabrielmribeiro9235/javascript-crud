4f7614fe2ba3a6335b3226a2c66f6c7e7360f3b0
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2025-12-29 22:49:57 -0300

    feat: add login field to login page and creat the function submitLogin

--- main.js
+++ main.js
@@ -29,3 +29,13 @@ const deletePost = (id) => {
     console.log("Post não encontrado!");
   }
 };
+
+const submitLogin = () => {
+  document.getElementById("loginForm").addEventListener("submit", event => {
+    event.preventDefault();
+    const userName = document.getElementById("userName").value;
+    const password = document.getElementById("password").value;
+    sessionStorage.setItem("user", JSON.stringify({ userName, password }));
+    window.location.href = "./main.html";
+  });
+}
