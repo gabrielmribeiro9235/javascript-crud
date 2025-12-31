ad3664cb87a28013608951cd482408ef34bddbbe
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2025-12-31 12:06:43 -0300

    refactor: change some styles and refactor the login.js

--- js/login.js
+++ js/login.js
@@ -1,9 +1,7 @@
-const submitLogin = () => {
-  document.getElementById("loginForm").addEventListener("submit", event => {
+document.getElementById("loginForm").addEventListener("submit", event => {
     event.preventDefault();
     const userName = document.getElementById("userName").value;
     const password = document.getElementById("password").value;
     sessionStorage.setItem("user", JSON.stringify({ userName, password }));
     window.location.href = "./main.html";
-  });
-}
+});
