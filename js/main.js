4cf19420be1aa6f99362639fa22bd3f282411762
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-01-01 19:19:34 -0300

    fix: use history.pushState to restore the main page instead of navigating back to login page

--- js/main.js
+++ js/main.js
@@ -36,6 +36,12 @@ document.getElementById("addPostBtn").addEventListener("click", event => {
   div.classList.add("showPopUp");
   const header = document.querySelector("header");
   header.style.display = "none";
+  history.pushState({ showPopUp: true }, "", "newPost");
+});
+
+window.addEventListener("popstate", (event) => {
+  location.reload();
+  history.pushState("main", "", "main.html");
 });
 
 const userName = JSON.parse(sessionStorage.getItem("user")).userName;
