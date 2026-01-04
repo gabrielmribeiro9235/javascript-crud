627137273cb12e94eee89f52acbc68ddc04f1d6b
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-01-04 12:04:55 -0300

    style: improve styles of delete and update buttons

--- js/main.js
+++ js/main.js
@@ -60,13 +60,29 @@ const addQrCodeToPage = () => {
       const deleteButton = document.createElement("button");
       const updateButton = document.createElement("button");
       deleteButton.id = "deleteButton";
-      updateButton.id = "updateButon";
-      deleteButton.textContent = "Excluir";
-      updateButton.textContent = "Alterar";
+      updateButton.id = "updateButton";
+      deleteButton.innerHTML = `
+      <span>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" stroke="white" stroke-width="0.8" class="bi bi-trash" viewBox="0 0 16 16">
+          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+          <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+        </svg>
+      </span>
+      <span>Excluir</span>`;
+      updateButton.innerHTML = `
+      <span>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" stroke="white" stroke-width="0.8" class="bi bi-repeat" viewBox="0 0 16 16">
+          <path d="M11 5.466V4H5a4 4 0 0 0-3.584 5.777.5.5 0 1 1-.896.446A5 5 0 0 1 5 3h6V1.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192m3.81.086a.5.5 0 0 1 .67.225A5 5 0 0 1 11 13H5v1.466a.25.25 0 0 1-.41.192l-2.36-1.966a.25.25 0 0 1 0-.384l2.36-1.966a.25.25 0 0 1 .41.192V12h6a4 4 0 0 0 3.585-5.777.5.5 0 0 1 .225-.67Z"/>
+        </svg>
+      </span>
+      <span>Alterar</span>`;
+      const buttons = document.createElement("div");
+      buttons.id = "buttons";
+      buttons.append(updateButton, deleteButton);
       const img = document.createElement("img");
       img.src = post.content;
       titleAndButtonsDiv.innerHTML = `<h2>${post.title}</h2>`;
-      titleAndButtonsDiv.append(updateButton, deleteButton);
+      titleAndButtonsDiv.append(buttons);
       imageDiv.append(img);
       div.append(imageDiv, titleAndButtonsDiv);
       document.querySelector("main").append(div); 
